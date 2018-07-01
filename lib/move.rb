e6010550458a792a57f1720426aa0461696b3851
Michael Hamilton <michael.ts.hamilton@gmail.com>
2018-07-01 12:01:48 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -8,11 +8,11 @@ end
 
 # code your input_to_index and move method here!
 
+def input_to_index(input)
+  return input.to_i - 1
+end
+
 def move(array, index, value = 'X')
   array[index] = value
   return board
-end
-
-def input_to_index(input)
-  return input.to_i - 1
 end
